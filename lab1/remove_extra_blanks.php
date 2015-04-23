b4babdba1446172705d17c88272480bc52a09ca3
ahmetgaraev <ahmetgaraev@gmailcom>
2015-04-23 21:59:07 +0300

    Добавлена часть лабораторной работы

--- lab1/remove_extra_blanks.php
+++ lab1/remove_extra_blanks.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type: text/plain");
-	$str_1 = $_GET['arg1'];
-	$str_2 = $_GET['arg2'];
-	$str_3 = $str_1 + $str_2;
-    echo $str_3;
-	//remove_extra_blanks.php?text=10
+	$str_1 = $_GET['text'];
+	$str_2 = trim($str_1, " ");
+	$str_2 = preg_replace('/[\s]{2,}/', ' ', $str_2);
+    echo $str_2;
+	//remove_extra_blanks.php?text=   hello   world   
