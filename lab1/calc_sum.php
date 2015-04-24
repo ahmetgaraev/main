ec6806510f521141efc0f49e2b8d48ea172fba01
ahmetgaraev <ahmetgaraev@gmailcom>
2015-04-24 21:36:45 +0300

    Правки в calc_sum.php

--- lab1/calc_sum.php
+++ lab1/calc_sum.php
@@ -1,7 +1,11 @@
 <?php
     header("Content-Type: text/plain");
-	$str_1 = $_GET['arg1'];
-	$str_2 = $_GET['arg2'];
-	$str_3 = $str_1 + $str_2;
+    $str_1 = $_GET['arg1'];
+    $str_2 = $_GET['arg2'];
+    $str_3 = 'Not value';
+    if (is_numeric($str_1) && is_numeric($str_2)) 
+    {
+        $str_3 = $str_1 + $str_2;
+    }
     echo $str_3;
-	//calc_sum.php?arg1=10&arg2=15
+    //calc_sum.php?arg1=10&arg2=15
