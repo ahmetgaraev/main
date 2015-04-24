a365d49ac78fd98bf3b187b4075af875f9028ca0
ahmetgaraev <ahmetgaraev@gmailcom>
2015-04-24 21:55:07 +0300

    правки

--- lab1/print_name.php
+++ lab1/print_name.php
@@ -1,7 +1,5 @@
 <?php
     header("Content-Type: text/plain");
-	$str_1 = "Hello, Dear ";
-	$str_2 = $_GET['name'];
-	$str_3 = "!";
-    echo $str_1.$str_2.$str_3;
+	$str_1 = "Hello, Dear " . $_GET['name'] . "!";
+    echo $str_1;
 	//http://127.0.0.1:4001/print_name.php?name=aaaaaaaaaaabc
