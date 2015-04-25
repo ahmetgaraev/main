84b858d6fefcd041aadf86ca6fa68397c3c0c91c
ahmetgaraev <ahmetgaraev@gmailcom>
2015-04-25 23:46:20 +0300

    правки

--- lab1/print_name.php
+++ lab1/print_name.php
@@ -1,5 +1,6 @@
 <?php
     header("Content-Type: text/plain");
-	$str_1 = "Hello, Dear " . $_GET['name'] . "!";
-    echo $str_1;
-	//http://127.0.0.1:4001/print_name.php?name=aaaaaaaaaaabc
+    if (isset($_GET['name']))
+    {
+        echo "Hello, Dear {$_GET['name']} !";
+    }
--- lab1/remove_extra_blanks.php
+++ lab1/remove_extra_blanks.php
@@ -1,7 +1,12 @@
 <?php
     header("Content-Type: text/plain");
-	$str_1 = $_GET['text'];
-	$str_2 = trim($str_1, " ");
-	$str_2 = preg_replace('/[\s]{2,}/', ' ', $str_2);
-    echo $str_2;
-	//remove_extra_blanks.php?text=   hello   world   
+    $arr = explode(" ", isset($_GET['text']) ? $_GET['text'] : '');
+    $result = '';
+    foreach($arr as $value)
+    {
+        if ($value) 
+        {
+            $result .= ' ' . $value;
+        }
+    }
+    echo trim($result);
