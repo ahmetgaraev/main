fcbc72df9b8636caa87c98c7add679521aa9be59
ahmetgaraev <ahmetgaraev@gmailcom>
2015-04-29 21:39:52 +0300

    Правки

--- lab1/calc_sum.php
+++ lab1/calc_sum.php
@@ -1,9 +1,6 @@
 <?php
     header("Content-Type: text/plain");
-    if (is_numeric($_GET["arg1"]) & is_numeric($_GET["arg2"])) { echo $_GET["arg1"] + $_GET["arg2"]; }
-
-    //if (isset($_GET["arg1"],$_GET["arg2"])) & (is_numeric($_GET["arg1"]) & is_numeric($_GET["arg2"])) { echo $_GET["arg1"] + $_GET["arg2"]; }
-
-    //$arg1 = isset($_GET["arg1"]) ? $_GET["arg1"] : 0;
-    //$arg2 = isset($_GET["arg2"]) ? $_GET["arg2"] : 0;
-    //echo $arg1 + $arg2;    
+    if (isset($_GET["arg1"], $_GET["arg2"])) && (is_numeric($_GET["arg1"]) && is_numeric($_GET["arg2"])) 
+    { 
+        echo $_GET["arg1"] + $_GET["arg2"]; 
+    }    
