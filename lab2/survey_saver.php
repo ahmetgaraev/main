21878a397ad8a7c13e1f5bebdd5c1df0ba5a1290
ahmetgaraev <ahmetgaraev>
2015-05-13 18:04:19 +0300

    111111111111

--- lab2/survey_saver.php
+++ lab2/survey_saver.php
@@ -1,26 +1,46 @@
 <?php
     header("Content-Type: text/plain");
     require_once('include/common.inc.php');
-    
-    $first_name = GetParamFromGet('first_name', '');
-    $last_name = GetParamFromGet('last_name', '');
-    $email = GetParamFromGet('email', '');
-    $age = GetParamFromGet('age', 0);
-    $age = is_int($age) ? is_int($age) : 0;
 
-    function SurveySaver($first_name, $last_name, $email, $age)
+    function GetSurveyFromRequest()
     {
-        $data = "First Name: {$first_name},\r\nLast Name: {$last_name},\r\nEmail: {$email},\r\nAge: {$age}";
-        if (!is_dir("data"))
+        $info = array
+        (
+            'first_name' => GetParamFromGet('first_name', ''),
+            'last_name' => GetParamFromGet('last_name', ''),
+            'email' => GetParamFromGet('email', ''),
+            'age' => GetParamFromGet('age', '')
+        );
+        return $info;
+    }
+
+    function CreateSurveyDir()
+    {
+        if ( !is_dir("data") )
         {
             mkdir("data");
         }
-        $f = fopen("data/{$email}.txt", "w");
-        fwrite($f, $data);
-        fclose($f);
     }
 
-    if ( strpos($email, '@')!==false ) 
+    function GetSurveyFilePath($email)
     {
-        SurveySaver($first_name, $last_name, $email, $age);
+        CreateSurveyDir();
+        return dirname(__FILE__) . '\\data\\' . $email . '.txt';
+    }
+
+    function SaveSurveyToFile($survey)
+    {
+        $path = GetSurveyFilePath($survey['email']);
+        $data = serialize($survey);
+
+        $handle = fopen($path, 'a');
+        fwrite($handle, $data);
+        fclose($handle);
+    }
+
+    $survey = GetSurveyFromRequest();
+
+    if ( !empty($survey) )
+    {
+         SaveSurveyToFile($survey);
     }
