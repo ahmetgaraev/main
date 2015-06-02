f5a6a105072239e0d26191093313fa530e3cfab1
ahmetgaraev <ahmetgaraev>
2015-06-02 12:48:46 +0300

    commit

--- lab4/image_upload.php
+++ lab4/image_upload.php
@@ -7,23 +7,32 @@
 
         $imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
 
-        if ( $imageinfo['mime'] != 'image/jpeg' ) 
+        $isImage = ( ($imageinfo['mime'] == 'image/jpeg') || ($imageinfo['mime'] == 'image/png') || ($imageinfo['mime'] == 'image/gif') );
+
+        if ( $isImage )
         {
-            $vars['msg'] = "Sorry, we only accept JPEG images\n";
+            $vars['msg'] = "File is image\n";
+        }
+        else
+        {
+            $vars['msg'] = "Sorry, we only accept png, jpeg, gif images\n";
             buildLayout('image_upload.html', $vars);
             exit;
         }
 
         $uploaddir = 'uploads/images/';
-        $uploadfile = $uploaddir . date('YmdHis') . rand(10,100) . '.jpeg';
+        $uploadfile = $uploaddir . date('YmdHis') . rand(10,100);
 
-        if ( move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile) )
+        if ( file_exists($uploaddir) ) 
         {
-            $vars['msg'] = "File is valid, and was successfully uploaded.\n";
-        } 
-        else 
-        {
-            $vars['msg'] = "File uploading failed.\n";
+            if ( move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile) )
+            {
+                $vars['msg'] = "File is valid, and was successfully uploaded.\n";
+            } 
+            else 
+            {
+                $vars['msg'] = "File uploading failed.\n";
+            }
         }
     }
 
